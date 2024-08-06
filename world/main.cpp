3022a726b9c84e46f01f1fbc79ffb632321fc043
githubsherly <sherlyphing99020@gmail.com>
2024-08-06 18:24:54 +0700

    Initial Commit

---
+++ world/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  world
+//
+//  Created by sherly phing on 06/08/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
